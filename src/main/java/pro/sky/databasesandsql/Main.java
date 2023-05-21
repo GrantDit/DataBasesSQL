3854adf9042162fd56e80579c27ebc2355909e19
GrantDit <kiruha5854@mail.ru>
2023-05-21 18:36:06 +0300

    First commit homework 1

---
+++ src/main/java/pro/sky/databasesandsql/Main.java
@@ -0,0 +1,7 @@
+package pro.sky.databasesandsql;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HomeWork 1");
+    }
+}
---
+++ src/test/java/pro/sky/databasesandsql/DataBasesAndSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.databasesandsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBasesAndSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
